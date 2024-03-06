86204003f8c91d6dc6cec90ea0181288ec663e87
ahrisai <sasaslapik40@gmail.com>
2024-03-06 10:37:39 +0300

    kp finished

--- src/controllers/PlayerController.ts
+++ src/controllers/PlayerController.ts
@@ -1,19 +1,93 @@
 import { PrismaClient } from '@prisma/client';
 import { Request, Response } from 'express';
 import bcrypt from 'bcrypt';
-import { secretKey } from '../config.js';
 const prisma = new PrismaClient();
 const saltRounds = 7;
+const playersPerPage = 5;
+
+async function getPlayers(
+  sortField: string,
+  sortOrder: string,
+  cs2_dataInclude: boolean,
+  description: boolean,
+  user_avatar: boolean,
+  gender: string,
+  pageNumber: number,
+  searchQuery: string,
+  filterQuery: string
+) {
+  const playersPerPage = 5,
+    skip = (pageNumber - 1) * playersPerPage;
+  const whereClause = {
+    nickname: { not: 'admin' },
+    gender: gender !== '' ? gender : {},
+    user_avatar: user_avatar ? { not: '' } : {},
+    description: description ? { not: '' } : {},
+    cs2_data: cs2_dataInclude ? { isNot: null } : {},
+    ...(searchQuery ? { [filterQuery]: { contains: searchQuery } } : {}),
+  };
+  return await prisma.user.findMany({
+    where: whereClause,
+    orderBy: sortField ? { [sortField]: sortOrder } : {},
+    include: {
+      cs2_data: {
+        include: {
+          roles: { select: { cs2Role: { select: { name: true } } } },
+          maps: { select: { cs2Map: { select: { name: true } } } },
+        },
+      },
+    },
+    skip: skip,
+    take: playersPerPage,
+  });
+}
+
+async function getTotalPages(
+  sortField: string,
+  sortOrder: string,
+  cs2_dataInclude: boolean,
+  description: boolean,
+  user_avatar: boolean,
+  gender: string
+) {
+  const playersPerPage = 5;
+  let orderByClause = sortField ? { [sortField]: sortOrder } : {};
+  let cs2_dataWith = cs2_dataInclude ? { isNot: null } : {};
+  let descClause = description ? { not: '' } : {};
+  let genderClause = gender !== '' ? gender : {};
+  let user_avatarClause = user_avatar ? { not: '' } : {};
+  const count = await prisma.user.count({
+    where: {
+      nickname: { not: 'admin' },
+      gender: genderClause,
+      user_avatar: user_avatarClause,
+      description: descClause,
+      cs2_data: cs2_dataWith,
+    },
+    orderBy: orderByClause,
+  });
+  const totalCount = count / playersPerPage;
+  console.log(totalCount);
+  if (totalCount > 1.2 && totalCount < 2) {
+    return Math.round(totalCount) + 1;
+  } else return Math.round(count / playersPerPage);
+}
+
 class PlayerController {
   updatePlayerById = async (req: Request, res: Response) => {
     try {
       if (typeof req.query.id === 'string') {
         const Uid = parseInt(req.query.id);
-        console.log(req.body);
-        await prisma.user.update({ where: { id: Uid }, data: req.body });
-        console.log('дошел');
-        const users = await prisma.user.findMany({ include: { cs2_data: true }, orderBy: { id: 'asc' } });
-        return res.status(200).json(users);
+
+        await prisma.user.updateMany({ where: { id: Uid }, data: req.body });
+
+        const players = await prisma.user.findMany({
+          where: { nickname: { not: 'admin' } },
+          orderBy: { id: 'asc' },
+          include: { cs2_data: true },
+        });
+
+        return res.status(200).json(players);
       }
     } catch (error) {
       console.log(error);
@@ -21,10 +95,40 @@ class PlayerController {
   };
 
   fetchAllPlayers = async (req: Request, res: Response) => {
-    try {
-      const players = await prisma.user.findMany({ include: { cs2_data: true }, orderBy: { id: 'asc' } });
-      return res.status(200).json(players);
-    } catch (error) {}
+    const { filter, sort, cs2_data, desc, user_avatar, gender, page, searchQuery, searchFilter } = req.query;
+    let bool_cs2_data: boolean;
+    let bool_desc: boolean;
+    let bool_user_avatar: boolean;
+
+    if (cs2_data && desc && page) {
+      bool_cs2_data = cs2_data === 'true';
+      bool_desc = desc === 'true';
+      bool_user_avatar = user_avatar === 'true';
+      const nPage: number = +page;
+      try {
+        const players = await getPlayers(
+          filter as string,
+          sort as string,
+          bool_cs2_data,
+          bool_desc,
+          bool_user_avatar,
+          gender as string,
+          nPage,
+          searchQuery as string,
+          searchFilter as string
+        );
+
+        const totalPages = await getTotalPages(
+          filter as string,
+          sort as string,
+          bool_cs2_data,
+          bool_desc,
+          bool_user_avatar,
+          gender as string
+        );
+        return res.status(200).json({ totalPages, players });
+      } catch (error) {}
+    }
   };
 
   deletePlayerById = async (req: Request, res: Response) => {
@@ -34,7 +138,11 @@ class PlayerController {
       await prisma.user.delete({ where: { id: id } });
     }
 
-    const players = await prisma.user.findMany({ include: { cs2_data: true } });
+    const players = await prisma.user.findMany({
+      where: { nickname: { not: 'admin' } },
+      orderBy: { id: 'asc' },
+      include: { cs2_data: true },
+    });
     res.status(200).json(players);
   };
 
@@ -84,14 +192,22 @@ class PlayerController {
         data: { ...user, password: hash },
       });
       const players = await prisma.user.findMany({
+        where: { nickname: { not: 'admin' } },
         orderBy: { id: 'asc' },
+        include: { cs2_data: true },
       });
-      console.log('аж сюда');
+
       res.status(200).json(players);
     } catch (error) {
       console.log(error);
       res.status(400).json({ message: 'reg error, try again' });
     }
+  };
+
+  totalPages = async (req: Request, res: Response) => {
+    const totalUsers = await prisma.user.count();
+    const totalPages = Math.round(totalUsers / playersPerPage);
+    res.status(200).json(totalPages);
   };
 }
 
